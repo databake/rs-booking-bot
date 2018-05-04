71673f6523e4dfc6e5e02894b8829b896e5b4a00
Graham Barker <graham.barker@donson.co.uk>
2018-05-04 12:54:50 +0100

    Changing the title

--- src/App.js
+++ src/App.js
@@ -294,7 +294,7 @@ class App extends Component {
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>RS Connect Agent</h1>
+          <h1 className='App-title'>RS Connect Booking Agent</h1>
         </header>
         <div style={{ margin: 20 }}>
           <ChatBot
