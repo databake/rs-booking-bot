52927a4d915ef1361c42fe13fd504f5a033ff6a6
Graham Barker <graham.barker@donson.co.uk>
2018-05-13 18:50:45 +0100

    Woowaaa

--- src/App.js
+++ src/App.js
@@ -291,13 +291,11 @@ class App extends Component {
     }
     const { steps } = this.state
     return (
-      <div className='App'>
-        <ChatBot
-          headerTitle={'RS Connect Booking Agent'}
-          floating
-          steps={steps}
-        />
-      </div>
+      <ChatBot
+        headerTitle={'RS Connect Booking Agent'}
+        floating
+        steps={steps}
+      />
     )
   }
 }
